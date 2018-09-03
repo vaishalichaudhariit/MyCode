90ef3ebc0fe9348cf8cab6bd31b54ff9ca4a7c2f
vaishalichaudhariit <vaishalichaudhariit@gmail.com>
2018-09-03 12:25:32 +0530

    commit on Git

---
+++ RestAssuredBDDFramework/src/test/java/com/qa/rest/tests/GetCallBDD.java
@@ -0,0 +1,27 @@
+package com.qa.rest.tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetCallBDD {
+
+	@Test
+	public void test_numberofCircuitsFor2017(){
+		/*given()
+		  when().
+		  then().
+		  assert()*/
+		given().
+		when().
+		get("http://ergast.com/api/f1/2017/circuits.json").
+		then().
+		assertThat().
+		statusCode(200).
+		and().
+		body("MRData.CircuitTable.Circuits.circuitId",hasSize(20)).
+		and().
+		header("content-length", equalTo("4551"));
+	}
+}
